c6cc5769b2c14cf5d0db2db7a89f3341272b8f6f
haleysong576 <53978880+haleysong576@users.noreply.github.com>
2020-06-21 15:42:50 -0400

    Update TutorProfile.java

--- java/TutorProfile.java
+++ java/TutorProfile.java
@@ -10,5 +10,33 @@ public class TutorProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutor_profile);
+        
+        ImageButton setting = (ImageButton)findViewById(R.id.setting);
+        setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent settingScreen = new Intent(getApplicationContext(), Setting.class);
+                startActivity(settingScreen);
+            }
+        });
+
+        ImageButton home = (ImageButton)findViewById(R.id.home);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent homeScreen = new Intent(getApplicationContext(), Home.class);
+                startActivity(homeScreen);
+            }
+        });
+
+        ImageButton chat = (ImageButton)findViewById(R.id.chat);
+        chat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent chatScreen = new Intent(getApplicationContext(), Chat.class);
+                startActivity(chatScreen);
+            }
+        });
+    }
     }
 }
