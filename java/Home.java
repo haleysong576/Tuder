346a8c6d7ad3e5629a72305990ff9b9a573278d5
haleysong576 <53978880+haleysong576@users.noreply.github.com>
2020-06-21 16:16:20 -0400

    Create Home.java

---
+++ java/Home.java
@@ -0,0 +1,44 @@
+package com.example.tuder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        ImageButton setting = (ImageButton)findViewById(R.id.setting);
+        setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent settingScreen = new Intent(getApplicationContext(), Setting.class);
+                startActivity(settingScreen);
+            }
+        });
+
+        ImageButton profile = (ImageButton)findViewById(R.id.profile);
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent profileScreen = new Intent(getApplicationContext(), TutorProfile.class);
+                startActivity(profileScreen);
+            }
+        });
+
+        ImageButton chat = (ImageButton)findViewById(R.id.chat);
+        chat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent chatScreen = new Intent(getApplicationContext(), Chat.class);
+                startActivity(chatScreen);
+            }
+        });
+    }
+}
