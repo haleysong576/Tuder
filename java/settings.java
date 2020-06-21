670b886799901221eddc68fb2059214a4164e907
haleysong576 <53978880+haleysong576@users.noreply.github.com>
2020-06-21 16:40:47 -0400

    Update settings.java

--- java/settings.java
+++ java/settings.java
@@ -10,5 +10,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        ImageButton profile = (ImageButton)findViewById(R.id.profile);
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent profileScreen = new Intent(getApplicationContext(), TutorProfile.class);
+                startActivity(profileScreen);
+            }
+        });
+
+        ImageButton chat = (ImageButton)findViewById(R.id.chat);
+        chat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent chatScreen = new Intent(getApplicationContext(), Chat.class);
+                startActivity(chatScreen);
+            }
+        });
+        
+        ImageButton home = (ImageButton)findViewById(R.id.home);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent homeScreen = new Intent(getApplicationContext(), Home.class);
+                startActivity(homeScreen);
+            }
+        });
     }
 }
